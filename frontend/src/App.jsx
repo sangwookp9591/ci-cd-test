782720229323f26d7697d7ed7cf5cdcb2c71afb6
sangwookp9591 <sangwookp9591@gmail.com>
2022-05-03 15:28:43 +0900

    api url 수정

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   const [count, setCount] = useState(0);
   function getHello() {
     const greet = document.getElementById("greet");
-    fetch(import.meta.env.VITE_API_SERVER + "/api/hello")
+    fetch(import.meta.env.VITE_API_SERVER + "/api/name")
       .then((response) => response.json())
       .then((data) => (greet.innerHTML = JSON.stringify(data)));
   }
